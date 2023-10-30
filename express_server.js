7d25bc27c36b0948a3a24047705df1bdc906f08c
UltraFerous <jandoga31@gmail.com>
2023-10-30 16:35:23 -0400

    Added newUrl input and random string generator

--- express_server.js
+++ express_server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.use(express.urlencoded({ extended: true }));
+
+const generateRandomString = function() {
+  let string = "";
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const charactersLength = characters.length;
+
+  for(let i = 0; i < 6; i++){
+    string += characters[(Math.floor(Math.random() * charactersLength))];
+  }
+  return string;
+};
 
 app.set("view engine", "ejs");
 
@@ -29,6 +41,11 @@ app.get("/urls", (req, res) => { //"/urls.json"
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/hello", (req, res) => {
