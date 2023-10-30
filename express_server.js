664f78d0e5dd8970269d30db14ecc463d2ab9ebd
UltraFerous <jandoga31@gmail.com>
2023-10-30 17:54:20 -0400

    Not sure but everything is working

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ const generateRandomString = function() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
 
-  for(let i = 0; i < 6; i++){
+  for (let i = 0; i < 6; i++) {
     string += characters[(Math.floor(Math.random() * charactersLength))];
   }
   return string;
@@ -25,12 +25,12 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.post("/urls", (req, res) => {
@@ -43,6 +43,11 @@ app.post("/urls", (req, res) => {
 
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
+  if (longURL === undefined) {
+    res.statusCode = 404;
+    res.send("404 Page Not Found.");
+    return;
+  }
   res.redirect(longURL);
 });
 
@@ -55,7 +60,6 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
-
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
