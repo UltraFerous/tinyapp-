94a9c940bcb13901b7fd7456b2609a6310bbef9e
UltraFerous <jandoga31@gmail.com>
2023-11-01 14:57:55 -0400

    Added register page

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-var cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
@@ -17,12 +17,59 @@ const generateRandomString = function() {
   return string;
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "a@b.com", //user@example.com
+    password: "123", //purple-monkey-dinosaur
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "c@d.ca", //user2@example.com
+    password: "123", //dishwasher-funk
+  },
+  undefined: {
+    id: undefined,
+    email: undefined, //user2@example.com
+    password: undefined, //dishwasher-funk
+  }
+};
+
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: users[req.cookies.id],
+  };
+  res.render("urls_register", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let userID = generateRandomString();
+
+  for(let keys in users){
+    if(users[keys].email === email){
+      res.send("Error: This account already exists.");
+    }
+  }
+  
+  users[userID] = {
+    id: userID.toString(),
+    email: email.toString(),
+    password: password.toString(), 
+  }
+
+  res.cookie('id', userID);
+  res.redirect(`/urls`);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -34,7 +81,7 @@ app.listen(PORT, () => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies.name,
+    username: users[req.cookies.id],
   };
   res.render("urls_new", templateVars);
 });
@@ -57,7 +104,7 @@ app.post("/login/", (req, res) => {
 });
 
 app.post("/logout/", (req, res) => {
-  res.clearCookie('name', req.body.username); 
+  res.clearCookie('id'); 
   res.redirect(`/urls/`);
 });
 
@@ -79,7 +126,7 @@ app.get("/u/:id", (req, res) => {
 app.get("/urls", (req, res) => { //"/urls.json"
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies.name,
+    username: users[req.cookies.id],
   };
   res.render("urls_index", templateVars);
 });
@@ -87,7 +134,7 @@ app.get("/urls", (req, res) => { //"/urls.json"
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id, longURL: urlDatabase[req.params.id],
-    username: req.cookies.name,
+    username: users[req.cookies.id],
   };
   res.render("urls_show", templateVars);
 });
